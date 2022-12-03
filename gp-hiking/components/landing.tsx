b111c35d432ed13673ca992af025d2babf982798
Loose37 <70845110+Loose37@users.noreply.github.com>
2022-12-03 13:19:08 +0900

    feat:adds landing page image. needs some styling to be fully functional

--- gp-hiking/components/landing.tsx
+++ gp-hiking/components/landing.tsx
@@ -1,7 +1,34 @@
-export const Landing = () => {
+import Image from 'next/image'
+import Link from 'next/link';
+
+  
+import { Button } from '@mui/material';
+import styles from "../styles/landing.module.css"
+
+export const Landing = ({}) => {
+    
     return (
-        <div>
-            Enter
-        </div>
+        <>
+            <div className='landing_background'
+            >
+                <Image
+                src={"/michael-chiara-QlY4oiFbT9o-unsplash.webp"}
+                alt="Background Image of some people walking a trail"
+                fill
+                objectFit='cover'
+                />
+            </div>
+            <h1
+            className={styles.h1}
+             >Hiking.Simplified
+             </h1>
+             <h2>
+                <Link href="/searchbypref">
+                    Start Walking
+                </Link>
+             </h2>
+        
+        
+        </>
     );
 }
--- gp-hiking/pages/index.tsx
+++ gp-hiking/pages/index.tsx
@@ -1,19 +1,20 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { Landing } from '../components'
 
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-<Head>
+    <div>
+      <Head>
         <title>Aruku</title>
         <meta name="description" content="hiking japan" />
         <link rel="icon" href="/boots.png" />
+        
       </Head>
       <Landing/>
 
+
     </div>
+
   )
 }
