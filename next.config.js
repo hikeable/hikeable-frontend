ff5136004cd185f72ceec5a6889c1bb55843f424
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-12-04 16:21:27 +0900

    redirect for logout page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+
+	async redirects() {
+		return [
+			{
+				source: "/logout",
+				destination: "/",
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
