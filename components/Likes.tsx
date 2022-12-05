1d764b0fa8fb5c170e853d8cff25bbafe5d5f82c
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-12-05 20:25:58 +0900

    feat: implement like button component

--- components/Likes.tsx
+++ components/Likes.tsx
@@ -1,15 +1,64 @@
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { IconButton } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type TrailLike = {
+  id: number;
+  user: number;
+  trail_id: number;
+  like: boolean;
+};
 
 export const Likes = () => {
   const [favorite, setFavorite] = useState<boolean>(false);
+  const [data, setData] = useState<TrailLike[]>([]);
 
-  const handleFavorite = () => {
-    if (!favorite) setFavorite(true);
-    else setFavorite(false);
+  const handleFavorite = async () => {
+    if (favorite) {
+      await axios({
+        method: "put",
+        url: "https://hikeable-backend.herokuapp.com/api/trails/likes/1",
+        data: {
+          id: 1,
+          user: 1,
+          trail_id: 1,
+          like: false,
+        },
+      });
+      setFavorite(false);
+    } else if (!favorite) {
+      await axios({
+        method: "put",
+        url: "https://hikeable-backend.herokuapp.com/api/trails/likes/1",
+        data: {
+          id: 1,
+          user: 1,
+          trail_id: 1,
+          like: true,
+        },
+      });
+      setFavorite(true);
+    }
   };
+
+  const fetchLikeData = async () => {
+    const fetchedLikeData = await axios.get(
+      "https://hikeable-backend.herokuapp.com/api/trails/1/likes"
+    );
+    setData(fetchedLikeData.data);
+  };
+
+  useEffect(() => {
+    fetchLikeData();
+  }, []);
+
+  useEffect(() => {
+    for (let object of data) {
+      if (object.user === 1 && object.like === true) setFavorite(true);
+    }
+  }, [data]);
 
   return (
     <>
