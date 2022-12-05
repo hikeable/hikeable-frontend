29da1d25acf90d0706fbc68e4a0f46ee084f73b4
haruna <haruna.kawakami867@gmail.com>
2022-12-05 13:25:47 +0900

    chore: fetch data from backend

--- pages/trails/[pref].tsx
+++ pages/trails/[pref].tsx
@@ -1,12 +1,27 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import useSWR from "swr";
 import { TrailCard } from "../../components";
 
 const _ = require("lodash");
 
+interface Trail {
+  id: number;
+}
+
+async function fetcher(key: string, init?: RequestInit) {
+  return fetch(key, init).then((res) => res.json() as Promise<Trail | null>);
+}
+
 const searchresults = () => {
   const router = useRouter();
   const { pref } = router.query;
+  const { data: trail, error } = useSWR(
+    "https://hikeable-backend.herokuapp.com/api/trail",
+    fetcher
+  );
+
+  console.log(trail);
 
   const capitalizePref = _.capitalize(pref as string);
 
