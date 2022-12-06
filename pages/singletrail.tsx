d4f51315882c126592fe6e627f00155513dc6bdf
haruna <haruna.kawakami867@gmail.com>
2022-12-06 16:43:49 +0900

    fix: fix JSON.parse bug

--- pages/singletrail.tsx
+++ pages/singletrail.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Trail } from "../global";
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
@@ -16,79 +17,88 @@ const difficultyObj = {
 
 const SingleTrail = () => {
   const router = useRouter();
-  const trail: Trail = JSON.parse(router.query.trail as string);
+  const [trail, setTrail] = useState<Trail | undefined>(undefined);
+  console.log(router.query.trail);
 
-  console.log(trail);
+  useEffect(() => {
+    if (router.query.trail !== undefined) {
+      setTrail(JSON.parse(router.query.trail as string));
+    } else {
+      return;
+    }
+  }, []);
 
   return (
-    <div style={{ width: "100%" }}>
-      <Box
-        sx={{
-          display: "flex",
-          m: 1,
-          p: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
+    trail && (
+      <div style={{ width: "100%" }}>
         <Box
           sx={{
             display: "flex",
-          }}
-        >
-          <Image src={""} alt="Placeholder" width={250} height={200} />
-          <Box
-            sx={{
-              flexDirection: "column",
-            }}
-          >
-            <Typography variant="h1">{trail.name}</Typography>
-            <Typography>{trail.prefecture}</Typography>
-            <Typography>{trail.length}</Typography>
-            <Typography>{difficultyObj[trail.difficulty]}</Typography>
-          </Box>
-        </Box>
-        <Box>
-          <Typography>5 Day Weather at Trail Name</Typography>
-        </Box>
-        <Box>
-          <Typography>Around Map at Trail Name</Typography>
-        </Box>
-        <Box
-          sx={{
-            flexDirection: "column",
-            width: "40%",
-            border: "solid",
-            borderRadius: "4px",
             m: 1,
             p: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
           }}
         >
           <Box
             sx={{
               display: "flex",
-              justifyContent: "space-between",
             }}
           >
-            <Typography>Tips for Trails</Typography>
-            <Typography>Add Tips</Typography>
+            <Image src={""} alt="Placeholder" width={250} height={200} />
+            <Box
+              sx={{
+                flexDirection: "column",
+              }}
+            >
+              <Typography variant="h1">{trail.name}</Typography>
+              <Typography>{trail.prefecture}</Typography>
+              <Typography>{trail.length}</Typography>
+              <Typography>{difficultyObj[trail.difficulty]}</Typography>
+            </Box>
+          </Box>
+          <Box>
+            <Typography>5 Day Weather at Trail Name</Typography>
+          </Box>
+          <Box>
+            <Typography>Around Map at Trail Name</Typography>
           </Box>
           <Box
             sx={{
-              marginTop: "1",
+              flexDirection: "column",
+              width: "40%",
               border: "solid",
               borderRadius: "4px",
+              m: 1,
+              p: 1,
             }}
           >
-            <Typography>From Haruna</Typography>
-            <Typography>
-              Stone stairs, and very slippery while and after raining!
-            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography>Tips for Trails</Typography>
+              <Typography>Add Tips</Typography>
+            </Box>
+            <Box
+              sx={{
+                marginTop: "1",
+                border: "solid",
+                borderRadius: "4px",
+              }}
+            >
+              <Typography>From Haruna</Typography>
+              <Typography>
+                Stone stairs, and very slippery while and after raining!
+              </Typography>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </div>
+      </div>
+    )
   );
 };
 
