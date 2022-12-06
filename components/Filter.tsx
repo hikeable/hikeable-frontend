ceabb473b77bbd7b930acfe462272eedd9f6de26
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-12-06 16:02:42 +0900

    update: styling changes to filter.tsx

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -3,12 +3,14 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Slider from '@mui/material/Slider';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Divider, List, ListItem } from '@mui/material';
 
 
 function valuetext(value: number) {
@@ -17,50 +19,16 @@ function valuetext(value: number) {
 
 
 function startFilter<trailProps>(trailArray : trailData[], lengthVal? : number [], difficultyVal? : string, locationVal? : string) {
-    // console.log(trailArray);
-    // console.log(difficultyVal);
-    // console.log(lengthVal);
-
-    let lookup = {
-        length: lengthVal,
-        difficulty: difficultyVal,
-        name: locationVal,
-
-    }
 
     const retTrails = trailArray.filter(isRequested);
-
-    
-
-
   
     function isRequested (trail : trailData) {
 
-            
-                // for (let key in lookup) {
-                //     console.log(key)
-                //     if (lookup.length){
-
-                //         (lookup.length)
-
-                //     }
-                // }
-                
-     
                 return ( 
 
                     (!locationVal ||  trail.name.toLowerCase().includes(locationVal.toLowerCase())) &&
                     (!lengthVal || parseInt(trail.length) >= lengthVal[0] && parseInt(trail.length) <= lengthVal[1]) &&
-                    (!difficultyVal || parseInt(difficultyVal) === trail.difficulty)
-
-
-
-                    
-                    // locationVal? (trail.name === locationVal):trail &&
-                    // lengthVal?(parseInt(trail.length) >= lengthVal[0] && parseInt(trail.length) <= lengthVal[1]):
-                    // trail
-                    
-                
+                    (!difficultyVal || parseInt(difficultyVal) === trail.difficulty)          
                 )
 
     }
@@ -105,12 +73,76 @@ export const Filter : React.FC<trailProps> = ({trails}:trailProps) => {
     const handleLocChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setLocation(event.target.value as string);
     };
+
+    const commonStyles = {
+        bgcolor: 'background.paper',
+        borderColor: 'text.primary',
+        m: 1,
+        border: 1,
+        width: '5rem',
+        height: '5rem',
+      };
     
 
 
     return (
 
+
         <>
+
+
+
+    {/* <RadioGroup aria-label="Difficulty levels" name="radio-buttons-group" defaultValue="Easy">
+        <List
+            sx={{
+            minWidth: 240,
+            '--List-gap': '0.5rem',
+            '--List-item-paddingY': '1rem',
+            '--List-item-radius': '8px',
+            '--List-decorator-size': '32px',
+            }}
+        >
+            {['Easy', 'Moderate', 'Hard'].map((item, index) => (
+            <ListItem
+                variant="outlined"
+                key={item}
+                sx={{ boxShadow: 'sm', bgcolor: 'background.body' }}
+            >
+                <ListItemDecorator>
+                {[index]}
+                </ListItemDecorator>
+                <Radio
+                    overlay
+                    value={index + 1}
+                    label={item}
+                    sx={{ flexGrow: 1, flexDirection: 'row-reverse' }}
+                    componentsProps={{
+                        action: ({ checked }) => ({
+                        sx: (theme) => ({
+                            ...(checked && {
+                            inset: -1,
+                            border: '2px solid',
+                            borderColor: theme.vars.palette.primary[500],
+                            }),
+                        }),
+                        }),
+                    }}
+                />
+            </ListItem>
+            ))}
+        </List>
+        </RadioGroup> */}
+
+        <TextField
+            helperText="Select a Trail"
+            id="demo-helper-text-aligned"
+            label="Trail"
+            onChange={handleLocChange}
+        />
+
+        <Divider />
+
+
 
         <FormControl>
             <FormLabel id="demo-radio-buttons-group-label">Difficulty</FormLabel>
@@ -126,11 +158,14 @@ export const Filter : React.FC<trailProps> = ({trails}:trailProps) => {
             </RadioGroup>
         </FormControl>
 
+        <Divider />
+
+
 
         <Typography id="length-slider" gutterBottom>
             Length
         </Typography>
-        <Box width={300} id = "length-slider">
+        <Box width= {300} id = "length-slider"> 
             <Slider 
                 getAriaLabel={() => 'Length'}
                 defaultValue={30} 
@@ -144,6 +179,7 @@ export const Filter : React.FC<trailProps> = ({trails}:trailProps) => {
             />
         </Box>
 
+        <Divider />
 
         <Box
             sx={{
@@ -152,12 +188,7 @@ export const Filter : React.FC<trailProps> = ({trails}:trailProps) => {
                 '& > :not(style)': { m: 1 },
             }}
          ></Box>
-         <TextField
-            helperText="Select a Trail"
-            id="demo-helper-text-aligned"
-            label="Trail"
-            onChange={handleLocChange}
-        />
+         
 
         <Button variant="contained" onClick={() => startFilter(trails, lengthVal, difficultyVal, locationVal )}>Filter</Button>
 
---
+++ gp-hiking/components/index.tsx
@@ -0,0 +1,7 @@
+export * from "./CompletedTrails";
+export * from "./Filter";
+export * from "./Landing";
+export * from "./Likes";
+export * from "./Navbar";
+export * from "./UserAuth";
+export * from "./TrailCard";
