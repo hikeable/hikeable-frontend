03646364c6825f1c5e30cdb15948a4e13d42b407
Loose37 <70845110+Loose37@users.noreply.github.com>
2022-12-06 15:40:19 +0900

    chore: merged conflicts with dev

--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,13 @@
+<<<<<<< Updated upstream
+=======
+export * from "./Landing";
+export * from "./CompletedTrails";
+export * from "./Filter";
+export * from "./Likes";
+export * from "./Navbar";
+export * from "./TrailCard";
+export * from "./context/UserAuth";
+>>>>>>> Stashed changes
 export * from "./Landing"
 export * from "./Navbar"
 export * from "./login"
