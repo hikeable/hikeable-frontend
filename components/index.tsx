4d0577c80530396e53a9f575ba325480017fde2b
Loose37 <70845110+Loose37@users.noreply.github.com>
2022-12-06 15:49:29 +0900

    chore: fix more unresolved conflict markers

--- components/index.tsx
+++ components/index.tsx
@@ -1,13 +1,9 @@
-<<<<<<< Updated upstream
-=======
-export * from "./Landing";
 export * from "./CompletedTrails";
 export * from "./Filter";
 export * from "./Likes";
 export * from "./Navbar";
 export * from "./TrailCard";
 export * from "./context/UserAuth";
->>>>>>> Stashed changes
-export * from "./Landing"
+export * from "./landing"
 export * from "./Navbar"
 export * from "./login"
