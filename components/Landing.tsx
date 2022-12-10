0f2c89bdf2234899baa9b5630f77edf2a3c53917
haruna <haruna.kawakami867@gmail.com>
2022-12-10 23:51:23 +0900

    feat: landing page styling for laptop

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -7,20 +7,30 @@ import styles from "../styles/landing.module.css";
 export const Landing = ({}) => {
   return (
     <>
-      <div className="landing_background">
+      <div className={styles.landing__background}>
         <Image
-          src={"/michael-chiara-QlY4oiFbT9o-unsplash.webp"}
+          src="/michael-chiara-QlY4oiFbT9o-unsplash.webp"
           alt="Background Image of some people walking a trail"
           fill
           objectFit="cover"
+          sizes="(max-width: 768px) 100vw,
+          (max-width: 1200px) 50vw,
+          33vw"
         />
       </div>
-
-      <h1 className={styles.h1}>Hiking.Simplified</h1>
-
-      <p className={styles.p}>
-        <Link href="/prefectures">Start Walking</Link>
-      </p>
+      <div className={styles.txt__wrapper}>
+        <h1 className={styles.landing__title}>Hiking.Simplified</h1>
+      </div>
+      <div className={styles.btn__wrapper}>
+        <Button variant="text" sx={{ mr: 2, ml: 1 }}>
+          <Link
+            className={`${styles.txt__btn} ${styles.btn__line}`}
+            href="/prefectures"
+          >
+            Start Walking
+          </Link>
+        </Button>
+      </div>
     </>
   );
 };
