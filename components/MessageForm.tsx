1541ac3ac08381447fe6565fadbab5612416fc2d
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-12-10 22:08:16 +0900

    feat: implement messageform textfield and buttons

--- components/MessageForm.tsx
+++ components/MessageForm.tsx
@@ -1,31 +1,51 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import StickyNote2 from "@mui/icons-material/StickyNote2";
 import TextField from "@mui/material/TextField";
 
 const MessageForm = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("Write your message here");
 
-  const handleFormOpen = () => {
-    setIsOpen(true);
-    // get current coordinates
+  const handleForm = () => {
+    if (!isOpen) {
+      setIsOpen(true);
+      // get GPS coordinates at this point
+    } else {
+      setIsOpen(false);
+    }
   };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // post request to back-end
+  }
 
   return (
     <>
       {isOpen === true ? (
-        <TextField
-          id="outlined-multiline-static"
-          label="Message"
-          multiline
-          rows={4}
-          placeholder="Write your message here"
-        />
+        <>
+          <TextField
+            id="outlined-multiline-static"
+            label="Message"
+            multiline
+            rows={4}
+            placeholder={value}
+            onChange={handleChange}
+          />
+          <Button variant="outlined" onClick={handleSubmit}>OK</Button>
+          <Button variant="outlined" onClick={handleForm}>
+            Back
+          </Button>
+        </>
       ) : (
         <Button
           variant="outlined"
           startIcon={<StickyNote2 />}
-          onClick={handleFormOpen}
+          onClick={handleForm}
         >
           Write Message
         </Button>
