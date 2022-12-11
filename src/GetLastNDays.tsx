088d769293f35a533c2c031d78ef3562066897da
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-12-11 22:42:37 +0900

    feat: create lastNdays fucntion

---
+++ src/GetLastNDays.tsx
@@ -0,0 +1,14 @@
+export default function getLastNDays(num) {
+    let dates : string[] = [];
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth();
+    
+    //loop through the last 7 days
+    for (let i = 0; i < num; i++) {
+    // subtract i days from today
+    let date = new Date(today.getFullYear(), month, day - i);
+    dates.push(`${date.getDate()}/${date.getMonth() + 1}`);
+    }
+    return dates;
+  }
