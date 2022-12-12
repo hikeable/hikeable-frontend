c069e435424606f620d386b40e4126e9db6c8a30
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-12-12 22:39:08 +0900

    chore: commit after pull on development

--- global.d.ts
+++ global.d.ts
@@ -42,6 +42,5 @@ declare module "leaflet-geosearch";
 declare module "react-leaflet";
 
 declare module "@mui/styles/defaultTheme" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface (remove this line if you don't have the rule enabled)
   interface DefaultTheme extends Theme {}
 }
