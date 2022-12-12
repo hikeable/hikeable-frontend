dc6ec94522c1b800c3da7b3f86aeb9e7925b4730
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-12-12 17:20:03 +0900

    chore: uninstall react-leaflet-locate-control

--- components/TrailMap.tsx
+++ components/TrailMap.tsx
@@ -4,7 +4,6 @@ import style from "../styles/singletrail.module.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import L from "leaflet";
-import LocateControl from "react-leaflet-locate-control";
 
 interface TrailMapProps {
   lat: string;
@@ -55,31 +54,32 @@ const TrailMap = ({ lat, lon, trailID }: TrailMapProps) => {
   }, []);
 
   return (
-    <MapContainer
-      className={style.map}
-      center={[latNumber, lonNumber]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {messageData.map((message, index) => {
-        const messageLatNumber = parseFloat(message.latitude);
-        const messageLonNumber = parseFloat(message.longitude);
-        return (
-          <Marker
-            key={index}
-            icon={leafletIcon}
-            position={[messageLatNumber, messageLonNumber]}
-          >
-            <Popup>{message.message}</Popup>
-          </Marker>
-        );
-      })}
-      {/* <LocateControl options={locateOptions} startDirectly /> */}
-    </MapContainer>
+    <>
+      <MapContainer
+        className={style.map}
+        center={[latNumber, lonNumber]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {messageData.map((message, index) => {
+          const messageLatNumber = parseFloat(message.latitude);
+          const messageLonNumber = parseFloat(message.longitude);
+          return (
+            <Marker
+              key={index}
+              icon={leafletIcon}
+              position={[messageLatNumber, messageLonNumber]}
+            >
+              <Popup>{message.message}</Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </>
   );
 };
 
