b746a3375a1ed610c71dd6daf04f480e11b00f6f
Loose37 <70845110+Loose37@users.noreply.github.com>
2022-12-13 17:38:45 +0900

    feat: add new environmetal variable for map in english API.

--- components/TrailMap.tsx
+++ components/TrailMap.tsx
@@ -70,7 +70,7 @@ const TrailMap = ({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url={`https://maptiles.p.rapidapi.com/en/map/v1/{z}/{x}/{y}.png?rapidapi-key=${process.env.NEXT_PUBLIC_MAP_API}`}
         />
         {messageData.map((message, index) => {
           const messageLatNumber = parseFloat(message.latitude);
