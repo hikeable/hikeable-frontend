f74de9f7ad8bfbab7b6d1294baf732d5b10434e8
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-12-14 10:49:03 +0900

    feat: basic styling for achivements page. folder for badges

--- pages/achievements.tsx
+++ pages/achievements.tsx
@@ -1,8 +1,14 @@
+import { Typography } from "@mui/material";
 import axios from "axios";
+import Image from 'next/image'
+import styles from "../styles/achievements.module.css"
+// import styles from "../styles/achievements.module.css";
 import { useAuthContext } from "../components/context/UseAuthContext";
 
 const badgeDict = {
-    "First Base":"/First Base.png"
+    "Incomplete": "/badges/Incomplete.png",
+    "First Base":"/badges/First Base.png",
+    "Power Of Two":"/badges/PowerOfTwo.png",
 
 }
 
@@ -21,15 +27,27 @@ const Achievements = () => {
     }
 
     return (
-        <><div>
-            Your Achievements
-        </div>
-        <Image
-            src= "/First Base.png"
-            // {badgeDict["First Base"]}
-            alt="First award"
-            fill
-        ></Image></>
+        <>
+            <div className={styles.txt_wrapper}>
+                <Typography variant="h1" className={styles.achivements_title}>
+                    Your Achievements
+                </Typography>
+            
+                <Typography variant="h2" className={styles.section_titles}>
+                    Getting Started
+                </Typography>
+            
+
+                <Image
+                    src= {badgeDict["First Base"]}
+                    alt="First Award"
+                    width={200}
+                    height={200}
+                />
+            
+            </div>
+
+        </>
         
     );
 }
