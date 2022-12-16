34eebf951febe733143657993bc88c6407d31528
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-12-16 17:02:44 +0900

    feat: successfully pass message details to modal

--- components/MessageDetails.tsx
+++ components/MessageDetails.tsx
@@ -1,19 +1,47 @@
 import { useEffect } from "react";
+import { Modal, Box, Typography } from "@mui/material";
 
 interface MessageRatingProps {
   messageDetails: Object;
   setMessageDetails: Function;
 }
 
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  cursor: "pointer",
+};
+
 const MessageDetails = ({
   messageDetails,
   setMessageDetails,
 }: MessageRatingProps) => {
-  useEffect(() => {
-    console.log(messageDetails);
-  }, [messageDetails]);
+  const handleClose = () => {
+    setMessageDetails(messageDetails["selected"] === false);
+  };
 
-  return <>Test</>;
+  return (
+    <Modal
+      keepMounted
+      open={messageDetails["selected"] === true}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography>{messageDetails["data"]["message"]}</Typography>
+      </Box>
+    </Modal>
+  );
 };
 
 export default MessageDetails;
