fdabe3c3c7ae992935744c2721fa2c56946abef7
haruna <haruna.kawakami867@gmail.com>
2022-12-17 12:50:02 +0900

    feat: weather change color and font-size

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -1,5 +1,4 @@
-import Head from "next/head";
-import Trail from "../global";
+import React from "react";
 import ReactWeather, { useVisualCrossing } from "react-open-weather";
 import styles from "../styles/weather.module.css";
 
@@ -20,22 +19,18 @@ export const Weather = ({ lon, lat, name }: WeatherProps) => {
     unit: "metric", // values are (metric, standard, imperial)
   });
 
-  // data?.forecast.unshift(data.forecast[0]);
-
-  // console.log(data);
-
   const customStyles = {
-    fontFamily: "Helvetica, sans-serif",
-    gradientStart: "#0181C2",
-    gradientMid: "#04A7F9",
-    gradientEnd: "#4BC4F7",
-    locationFontColor: "#FFF",
-    todayTempFontColor: "#FFF",
-    todayDateFontColor: "#B5DEF4",
-    todayRangeFontColor: "#B5DEF4",
-    todayDescFontColor: "#B5DEF4",
-    todayInfoFontColor: "#B5DEF4",
-    todayIconColor: "#FFF",
+    fontFamily: "Montserrat, sans-serif",
+    gradientStart: "#FFF",
+    gradientMid: "#FFF",
+    gradientEnd: "#FFF",
+    locationFontColor: "#777",
+    todayTempFontColor: "#777",
+    todayDateFontColor: "#777",
+    todayRangeFontColor: "#777",
+    todayDescFontColor: "#777",
+    todayInfoFontColor: "#777",
+    todayIconColor: "#4BC4F7",
     forecastBackgroundColor: "#FFF",
     forecastSeparatorColor: "#DDD",
     forecastDateColor: "#777",
@@ -53,7 +48,8 @@ export const Weather = ({ lon, lat, name }: WeatherProps) => {
           errorMessage={errorMessage}
           data={data}
           lang="en"
-          locationLabel={`${name}`}
+          // locationLabel={`${name}`}
+          locationLabel="Today"
           unitsLabels={{ temperature: "C", windSpeed: "Km/h" }}
           showForecast
         />
