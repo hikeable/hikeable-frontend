57683b6447714c6b91eb201de63f949b20059562
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-12-18 16:53:34 +0900

    style: add grid container to infopage

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
+import Image from "next/image";
 
 const About = () => {
   return (
@@ -13,14 +14,44 @@ const About = () => {
         justifyContent={"center"}
         alignItems={"center"}
       >
-        <Typography variant={"caption"}>About us</Typography>
-        <Typography variant={"h4"} mt={2} mb={1}>
+        <Typography variant="caption">About us</Typography>
+        <Typography variant="h4" my={2}>
           Built by adventurers, for adventurers.
         </Typography>
-        <Typography variant={"subtitle1"}>In a country where 80% of its landmass is mountainous, our mission is to provide a platform for both new and experienced hikers in Japan.</Typography>
+        <Typography variant="subtitle1">
+          In a country where 80% of its landmass is mountainous, our mission is
+          to provide a platform for both new and experienced hikers in Japan.
+        </Typography>
+      </Box>
+      <Box
+        p={4}
+        bgcolor={"white"}
+        marginLeft={"auto"}
+        marginRight={"auto"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Typography variant="subtitle2">Hikeable was built using:</Typography>
+        <Box></Box>
       </Box>
       <Box bgcolor={"lightgray"}>
-        <Typography>Our Team</Typography>
+        <Typography variant="h4">Our Team</Typography>
+
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
+          <Grid item xs={3}>
+            Cris
+          </Grid>
+          <Grid item xs={3}>
+            Kamil
+          </Grid>
+          <Grid item xs={3}>
+            Haruna
+          </Grid>
+          <Grid item xs={3}>
+            Chad
+          </Grid>
+        </Grid>
       </Box>
     </>
   );
