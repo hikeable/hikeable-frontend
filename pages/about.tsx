619fc36db3d3a4f0203018f574237bf4029a7dde
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-12-18 20:34:17 +0900

    style: add tech badges to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,7 @@ import {
   Grid,
   Card,
   CardContent,
+  CardActions,
   CardMedia,
   IconButton,
 } from "@mui/material";
@@ -13,11 +14,21 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import * as React from "react";
 
 const About = () => {
+  const techStackPictures = [
+    "/about/nextjs.svg",
+    "/about/typescript.svg",
+    "/about/mui.svg",
+    "/about/reactleaflet.svg",
+    "/about/django.svg",
+    "/about/postgresql.svg",
+  ];
+
   const staffCardInfo = [
     {
       name: "Cris A.",
       title: "Product Owner",
       github: "https://github.com/Loose37",
+      linkedin: "https://www.linkedin.com/in/cristian-armbruster",
     },
     {
       name: "Kamil B.",
@@ -33,7 +44,7 @@ const About = () => {
       name: "Chad G.",
       title: "Full-Stack Engineer",
       github: "https://github.com/chadgrover",
-      li: "https://www.linkedin.com/in/chadgrover/",
+      linkedin: "https://www.linkedin.com/in/chadgrover/",
     },
   ];
 
@@ -59,37 +70,69 @@ const About = () => {
         </Typography>
       </Box>
       <Box
+        
+        bgcolor={"white"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Typography variant="subtitle2">Hikeable was built using:</Typography>
+      </Box>
+      <Box
         p={4}
         bgcolor={"white"}
-        marginLeft={"auto"}
-        marginRight={"auto"}
         display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        textAlign={"center"}
+      >
+        <Grid container rowSpacing={1} columnSpacing={4}>
+          {techStackPictures.map((picture) => (
+            <Grid item xs={2} margin={"auto"}>
+              <Image src={picture} alt="Badge" width="64" height="64"></Image>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+      <Box
+        bgcolor={"lightgray"}
+        p={4}
+        display={"flex"}
+        flexDirection={"column"}
         justifyContent={"center"}
         alignItems={"center"}
       >
-        <Typography variant="subtitle2">Hikeable was built using:</Typography>
-        <Box></Box>
-      </Box>
-      <Box bgcolor={"lightgray"} p={4}>
-        <Typography variant="h4">Our Team</Typography>
+        <Typography variant="h4" my={2}>
+          Our Team
+        </Typography>
 
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
+        <Grid container rowSpacing={1} columnSpacing={{ md: 4 }}>
           {staffCardInfo.map((staff) => (
-            <Grid item xs={3}>
+            <Grid item xs={3} sx={{ textAlign: "center" }}>
               <Card variant="outlined">
-                <CardMedia component="img" alt="Staff Picture" />
-                <Typography fontSize={24} textAlign="center">
-                  {staff.name}
-                </Typography>
-                <Typography fontSize={16} textAlign={"center"}>
-                  {staff.title}
-                </Typography>
-                <IconButton href={staff.github}>
-                  <GitHubIcon />
-                </IconButton>
-                <IconButton href={staff.li}>
-                  <LinkedInIcon />
-                </IconButton>
+                <CardMedia
+                  sx={{
+                    height: "24vh",
+                    width: "24vh",
+                    p: 2,
+                    borderRadius: "50%",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                  }}
+                  component="img"
+                  alt="Profile Picture"
+                  image="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                />
+                <Typography fontSize={24}>{staff.name}</Typography>
+                <Typography fontSize={16}>{staff.title}</Typography>
+                <CardActions>
+                  <IconButton href={staff.github}>
+                    <GitHubIcon />
+                  </IconButton>
+                  <IconButton href={staff.linkedin}>
+                    <LinkedInIcon />
+                  </IconButton>
+                </CardActions>
               </Card>
             </Grid>
           ))}
