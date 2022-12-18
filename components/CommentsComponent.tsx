6b91a1f93c3c1dfac40f74ef2c54b137e0e74281
haruna <haruna.kawakami867@gmail.com>
2022-12-18 15:27:50 +0900

    feat: delete unnecessary lines & get comment out

--- components/CommentsComponent.tsx
+++ components/CommentsComponent.tsx
@@ -1,40 +1,46 @@
-import React, { useEffect,useState } from 'react';
-import { Paper, Button, Modal, TextField, Box, List, ListItem, Dialog, DialogContent, ListItemText} from '@mui/material';
+import React, { useEffect, useState } from "react";
+import {
+  Paper,
+  Button,
+  Modal,
+  TextField,
+  Box,
+  List,
+  ListItem,
+  Dialog,
+  DialogContent,
+  ListItemText,
+} from "@mui/material";
 import { Typography } from "@mui/joy";
-import axios from 'axios';
-import { useAuthContext } from './context/UseAuthContext';
-
-
+import axios from "axios";
+import { useAuthContext } from "./context/UseAuthContext";
 
 interface ScrollableTextProps {
   trailID: number;
 }
 
 type commentsDataObject = {
-  id: number,
-  user: number,
-  userName: string
-  trail_id: number,
-  comment: string,
-  date: string
-}
+  id: number;
+  user: number;
+  userName: string;
+  trail_id: number;
+  comment: string;
+  date: string;
+};
 
-const ScrollableText = ({
-  trailID}: ScrollableTextProps) => {
-  const [value,setValue] = useState("")
+const ScrollableText = ({ trailID }: ScrollableTextProps) => {
+  const [value, setValue] = useState("");
   const [comments, setComments] = useState<commentsDataObject[]>([]);
   const [modalOpen, setModalOpen] = useState(false);
-  const {user,userId} = useAuthContext()
+  const { user, userId } = useAuthContext();
 
   const userNameTag = user?.displayName;
-  let firstName
-  if (userNameTag){
-    const split =userNameTag.split(" ")
-    firstName =  split[0]
+  let firstName;
+  if (userNameTag) {
+    const split = userNameTag.split(" ");
+    firstName = split[0];
   }
 
-  
-  
   const style = {
     position: "absolute" as "absolute",
     top: "50%",
@@ -64,15 +70,14 @@ const ScrollableText = ({
 
   const handleSubmit = async () => {
     let current = new Date();
-    console.log ("userId =",userId)
-    console.log ("trail_id =",trailID)
-    console.log ("username =",firstName)
+    console.log("userId =", userId);
+    console.log("trail_id =", trailID);
+    console.log("username =", firstName);
 
-    console.log ("date =",  `${current.getFullYear()}-${
-      current.getMonth() + 1
-    }-${current.getDate()}`)
-
-    // https://hikeable-backend.herokuapp.com/api/trails/comments
+    console.log(
+      "date =",
+      `${current.getFullYear()}-${current.getMonth() + 1}-${current.getDate()}`
+    );
 
     await axios({
       method: "post",
@@ -81,38 +86,32 @@ const ScrollableText = ({
         user: userId,
         userName: firstName,
         trail_id: trailID,
-        comment:value,
+        comment: value,
         date: `${current.getFullYear()}-${
           current.getMonth() + 1
         }-${current.getDate()}`,
       },
     });
-  
+
     setValue("");
     handleModalClose();
   };
 
+  useEffect(() => {
+    fetchComments();
+  }, []);
 
-  useEffect(()=>{
-    fetchComments()
-  },[])
-
-  // "api/trails/<int:pk>/comments"
-
-  // https://hikeable-backend.herokuapp.com/api/trails/${trailID}/comments
   const fetchComments = async () => {
     const fetchedCommentsData = await axios.get(
       `https://hikeable-backend.herokuapp.com/api/trails/${trailID}/comments`
     );
-    console.log (fetchedCommentsData.data)
+    console.log(fetchedCommentsData.data);
     if (!comments) {
       setComments(fetchedCommentsData.data);
     } else {
-      
       setComments([...fetchedCommentsData.data]);
     }
   };
-
 
   const SubmitButton = () => {
     return (
@@ -120,11 +119,10 @@ const ScrollableText = ({
         variant="contained"
         disableElevation
         style={{ cursor: "pointer", zIndex: 99 }}
-        onClick={()=>{
-          handleSubmit() 
-          fetchComments()
-          fetchComments()
-
+        onClick={() => {
+          handleSubmit();
+          fetchComments();
+          fetchComments();
         }}
         onTouchStart={handleSubmit}
       >
@@ -137,63 +135,50 @@ const ScrollableText = ({
     <>
       <Button onClick={handleModalOpen}>Add a comment</Button>
       <Modal
-      keepMounted
-      open={modalOpen}
-      onClose={handleModalClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-        <Typography id="modal-modal-title"  component="h2">
-          Write Trail Comment
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2, mb: 2 }}>
-          Messages that condone violence, hate speech, harmful misinformation,
-          or criminal acts are subject to deletion, and the offending user will
-          be banned.
-        </Typography>
-        <TextField
-          sx={{ width: 1, mb: 2 }}
-          id="outlined-multiline-static"
-          label="Message"
-          multiline
-          rows={3}
-          placeholder={"Your message here"}
-          value={value}
-          onChange={handleTextChange}
-          InputProps={{ endAdornment: <SubmitButton /> }}
-        />
-      
-      </Box>
-    </Modal>
-      <Paper elevation={1} style={{ overflowY: 'scroll', height: '100px', width: "48vw" }}>
-           
-        <Typography>Comments for Trail</Typography>
+        keepMounted
+        open={modalOpen}
+        onClose={handleModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" component="h2">
+            Write Trail Comment
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2, mb: 2 }}>
+            Messages that condone violence, hate speech, harmful misinformation,
+            or criminal acts are subject to deletion, and the offending user
+            will be banned.
+          </Typography>
+          <TextField
+            sx={{ width: 1, mb: 2 }}
+            id="outlined-multiline-static"
+            label="Message"
+            multiline
+            rows={3}
+            placeholder={"Your message here"}
+            value={value}
+            onChange={handleTextChange}
+            InputProps={{ endAdornment: <SubmitButton /> }}
+          />
+        </Box>
+      </Modal>
+      <Paper
+        elevation={1}
+        style={{ overflowY: "scroll", height: "100px", width: "48vw" }}
+      >
         <List>
-        {comments.map((comment) => (
-          <ListItemText key={comment.id}> * {comment.date} {comment.userName} {comment.comment} {comment.trail_id}</ListItemText>
-      ))}
-      
-       
-
-        {/* {value} */}
-
+          {comments.map((comment) => (
+            <ListItemText key={comment.id}>
+              {" "}
+              {comment.date} {comment.userName} {comment.comment}{" "}
+              {comment.trail_id}
+            </ListItemText>
+          ))}
         </List>
-          
       </Paper>
-      {/* <Dialog open={open} onClose={handleClose}>
-  <DialogContent>
-    <List style={{ maxHeight: '50vh', overflow: 'auto' }}>
-      {commentss.map((comment) => (
-        <ListItem key={comment.id}>{comment.text}</ListItem>
-      ))}
-    </List>
-  </DialogContent>
-</Dialog> */}
-    
     </>
   );
 };
 
-
-export default ScrollableText
+export default ScrollableText;
