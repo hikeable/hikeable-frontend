eadba10da6cbe1a459be4c5c824f4abed18d6b21
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-12-18 18:17:00 +0900

    style: implement iconbuttons on about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,24 +1,39 @@
-import { Box, Typography, Grid, Card, CardContent } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+} from "@mui/material";
 import Image from "next/image";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import * as React from "react";
 
 const About = () => {
   const staffCardInfo = [
     {
-      name: "Cris",
+      name: "Cris A.",
       title: "Product Owner",
+      github: "https://github.com/Loose37",
     },
     {
-      name: "Kamil",
+      name: "Kamil B.",
       title: "Tech Lead",
+      github: "https://github.com/Tricole",
     },
     {
-      name: "Haruna",
+      name: "Haruna K.",
       title: "Full-Stack Engineer",
+      github: "https://github.com/harunakawakami",
     },
     {
-      name: "Chad",
+      name: "Chad G.",
       title: "Full-Stack Engineer",
+      github: "https://github.com/chadgrover",
+      li: "https://www.linkedin.com/in/chadgrover/",
     },
   ];
 
@@ -55,13 +70,27 @@ const About = () => {
         <Typography variant="subtitle2">Hikeable was built using:</Typography>
         <Box></Box>
       </Box>
-      <Box bgcolor={"lightgray"}>
+      <Box bgcolor={"lightgray"} p={4}>
         <Typography variant="h4">Our Team</Typography>
 
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
           {staffCardInfo.map((staff) => (
             <Grid item xs={3}>
-              <Card variant="outlined">{staff.name}</Card>
+              <Card variant="outlined">
+                <CardMedia component="img" alt="Staff Picture" />
+                <Typography fontSize={24} textAlign="center">
+                  {staff.name}
+                </Typography>
+                <Typography fontSize={16} textAlign={"center"}>
+                  {staff.title}
+                </Typography>
+                <IconButton href={staff.github}>
+                  <GitHubIcon />
+                </IconButton>
+                <IconButton href={staff.li}>
+                  <LinkedInIcon />
+                </IconButton>
+              </Card>
             </Grid>
           ))}
         </Grid>
