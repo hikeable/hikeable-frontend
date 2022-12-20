152cf5fc1d5555ad572c67b1067359f009502dd0
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-12-20 10:24:59 +0900

    style: change smallmap and largemap marker icons

--- components/SmallMap.tsx
+++ components/SmallMap.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import style from "../styles/singletrail.module.css";
 import L from "leaflet";
@@ -12,10 +12,10 @@ const SmallMap = ({ lat, lon }: SmallMapProps) => {
   const latNumber = parseFloat(lat);
   const lonNumber = parseFloat(lon);
   const trailHeadIcon = L.icon({
-    iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
+    iconUrl: "/trailHeadIcon.png",
     iconSize: [25, 41],
     iconAnchor: [10, 41],
-    shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png",
+    shadowUrl: "/markerShadow.png",
     popupAnchor: [2, -40],
   });
 
@@ -31,7 +31,9 @@ const SmallMap = ({ lat, lon }: SmallMapProps) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url={`https://maptiles.p.rapidapi.com/en/map/v1/{z}/{x}/{y}.png?rapidapi-key=${process.env.NEXT_PUBLIC_MAP_API}`}
         />
-        <Marker position={[latNumber, lonNumber]} icon={trailHeadIcon}></Marker>
+        <Marker position={[latNumber, lonNumber]} icon={trailHeadIcon}>
+          <Popup>Trail begins here</Popup>
+        </Marker>
       </MapContainer>
     </>
   );
