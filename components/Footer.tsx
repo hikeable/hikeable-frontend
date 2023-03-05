5dc765c4b21cf93a8d3cee51df7884921412f9b5
centrino.kib@gmail.com <centrino.kib@gmail.com>
2023-03-05 11:01:50 +0900

    Add basic footer with copyright + year each page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,16 @@
 import React from "react"
+import styles from "../styles/footer.module.css";
+
 
 export const Footer = () => {
 
     const year = new Date().getFullYear();
 
-    return <footer>{`Copyright © Hikeable ${year}`}</footer>;
+    return (
+
+            <footer className={styles.footer_style}>{`Copyright © Hikeable ${year}`}</footer>
+
+        
+    );
 
 }
