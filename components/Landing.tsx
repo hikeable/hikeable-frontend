9152e72efb1de0a777d8cebf3a4b4ea855005837
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-07-04 16:44:45 +0900

    style: add video background, new logo to landing page

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,14 +1,37 @@
+import React, { useState, useEffect } from "react";
+import Mountain from "../public/mountain.svg";
+
 function Landing() {
-    return (
-        <div className="bg-black">
-            <h1 className="landing-title">Welcome to the <span className="landing-title-accent">MERN</span> Stack</h1>
-            <h2 className="landing-subtitle">A simple MERN stack boilerplate</h2>
-            <div className="landing-buttons">
-                <a href="/login" className="landing-button">Login</a>
-                <a href="/register" className="landing-button">Register</a>
-            </div>
+  const [isClear, setIsClear] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsClear(true);
+    }, 1000);
+  }, []);
+
+  return (
+    <div
+      className={`${
+        isClear ? "opacity-100" : "opacity-0"
+      } w-screen h-screen transition-opacity duration-1000 ease-in-out flex justify-center items-center`}
+    >
+      <video
+        className="w-full h-full object-none"
+        src="/videos/splash.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className="grid grid-cols-2 absolute w-1/2 h-1/2 top-50 left-50 bg-white rounded-xl shadow-md opacity-90">
+        <div className="flex flex-col justify-center items-center">
+          <Mountain className="w-1/3 h-1/3 fill-lime-900 drop-shadow-md" />
+          <span className="text-6xl font-bold text-lime-900 drop-shadow-md">Hikeable</span>
         </div>
-    );
+        <p>Test 2</p>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
