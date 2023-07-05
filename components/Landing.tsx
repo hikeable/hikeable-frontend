eb196c566c396e2f1576dd3b71a4f4cf7b688d62
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-07-05 12:37:49 +0900

    feat: add flavor text to landing page

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -2,20 +2,21 @@ import React, { useState, useEffect } from "react";
 import Mountain from "../public/mountain.svg";
 
 function Landing() {
-  const [isClear, setIsClear] = useState(false);
+  const [isDivVisible, setIsDivVisible] = useState(false);
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsClear(true);
+    const divTimer = setTimeout(() => {
+      setIsDivVisible(true);
     }, 1000);
+
+    return () => {
+      clearTimeout(divTimer);
+    };
   }, []);
 
   return (
-    <div
-      className={`${
-        isClear ? "opacity-100" : "opacity-0"
-      } w-screen h-screen transition-opacity duration-1000 ease-in-out flex justify-center items-center`}
-    >
+    <div className="w-screen h-screen flex justify-center items-center">
       <video
         className="w-full h-full object-none"
         src="/videos/splash.mp4"
@@ -23,12 +24,24 @@ function Landing() {
         loop
         muted
       />
-      <div className="grid grid-cols-2 absolute w-1/2 h-1/2 top-50 left-50 bg-white rounded-xl shadow-md opacity-90">
+      <div
+        className={`${
+          isDivVisible ? "opacity-90" : "opacity-0"
+        } transition-opacity duration-1000 border-lime-800 border-8 ease-in-out grid grid-cols-2 absolute w-1/2 h-1/2 top-50 left-50 bg-white rounded-xl`}
+      >
         <div className="flex flex-col justify-center items-center">
-          <Mountain className="w-1/3 h-1/3 fill-lime-900 drop-shadow-md" />
-          <span className="text-6xl font-bold text-lime-900 drop-shadow-md">Hikeable</span>
+          <Mountain className="w-1/3 h-1/3 fill-lime-800 drop-shadow-md" />
+          <span className="text-6xl font-bold text-lime-800 drop-shadow-md">
+            Hikeable
+          </span>
         </div>
-        <p>Test 2</p>
+        <div className="flex flex-col justify-center items-center">
+          <p>
+            Discover the untamed beauty of Japan like never before with Hikeable
+            - the ultimate hiking companion that connects you with fellow
+            adventurers and lets you leave trailside messages along the way.
+          </p>
+        </div>
       </div>
     </div>
   );
