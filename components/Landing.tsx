4e8be473aa51248a163f429a4e78d390fea98866
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-07-05 15:03:15 +0900

    style: finish basic styling for new landing

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -10,8 +10,13 @@ function Landing() {
       setIsDivVisible(true);
     }, 1000);
 
+    const buttonTimer = setTimeout(() => {
+      setIsButtonVisible(true);
+    }, 2000);
+
     return () => {
       clearTimeout(divTimer);
+      clearTimeout(buttonTimer);
     };
   }, []);
 
@@ -26,21 +31,32 @@ function Landing() {
       />
       <div
         className={`${
-          isDivVisible ? "opacity-90" : "opacity-0"
-        } transition-opacity duration-1000 border-lime-800 border-8 ease-in-out grid grid-cols-2 absolute w-1/2 h-1/2 top-50 left-50 bg-white rounded-xl`}
+          isDivVisible ? "opacity-100" : "opacity-0"
+        } transition-opacity duration-500 ease-in-out grid grid-cols-2 absolute w-2/5 h-1/4 top-50 left-50 bg-lime-800 rounded-2xl`}
       >
         <div className="flex flex-col justify-center items-center">
-          <Mountain className="w-1/3 h-1/3 fill-lime-800 drop-shadow-md" />
-          <span className="text-6xl font-bold text-lime-800 drop-shadow-md">
+          <Mountain className="w-1/3 h-1/3 fill-slate-100 drop-shadow-md" />
+          <span className="text-6xl font-bold text-slate-100 drop-shadow-md">
             Hikeable
           </span>
         </div>
         <div className="flex flex-col justify-center items-center">
-          <p>
-            Discover the untamed beauty of Japan like never before with Hikeable
-            - the ultimate hiking companion that connects you with fellow
-            adventurers and lets you leave trailside messages along the way.
-          </p>
+          <div className="px-4">
+            <p className="leading-relaxed text-md text-slate-100 font-medium">
+              Experience Japan&apos;s beauty with <strong>Hikeable</strong> -
+              the ultimate hiking app. Connect with fellow adventurers, leave
+              trailside messages, and uncover new horizons.
+            </p>
+          </div>
+          <div>
+            <button
+              className={`${
+                isButtonVisible ? "opacity-100" : "opacity-0"
+              } mt-5 text-lg rounded-xl font-semibold drop-shadow-md cursor-pointer bg-slate-100 text-lime-800 px-8 py-2 transition-all duration-500 ease-in-out border-2 hover:bg-lime-800 hover:text-slate-100 hover:border-slate-100`}
+            >
+              Get Started
+            </button>
+          </div>
         </div>
       </div>
     </div>
