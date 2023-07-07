29a5fd6c4e095582f44025a92c789137f7bc8c00
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-07-07 12:50:27 +0900

    style: polish up xl size media queries

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -27,41 +27,44 @@ function Landing() {
         muted
       />
       <div
-        className="absolute grid grid-cols-1 xl:grid-cols-2
-        w-11/12 md:w-3/4 lg:w-1/2
+        className="absolute grid grid-cols-1 lg:grid-cols-2
+        w-11/12 md:w-2/3 lg:w-3/4 xl:w-1/2
         h-max
         gap-4 px-4 py-8
        bg-lime-800 rounded-2xl
-       top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+       top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 drop-shadow-md"
       >
         <div className="flex flex-row gap-2 justify-center items-center">
           <Mountain
             className="
           w-24 h-24
-          md:w-32 md:h-32
-          xl:w-24 lg:h-24
+          md:w-24 md:h-24
           
           fill-slate-100 drop-shadow-md"
           />
-          <span className="
+          <span
+            className="
           hidden
           md:inline-block
           md:text-6xl
-          xl:text-6xl
+          lg:text-4xl
 
-          font-bold text-slate-100 drop-shadow-md tracking-wide">
+          font-bold text-slate-100 drop-shadow-md tracking-wide"
+          >
             Hikeable
           </span>
         </div>
-        <div className="flex flex-col gap-6 justify-start xl:justify-center items-center text-slate-100">
+        <div className="flex flex-col gap-6 justify-start lg:justify-center items-center text-slate-100">
           <header>
-            <h1 className="text-base md:text-2xl xl:text-xl
-            font-bold tracking-wide">
+            <h1
+              className="text-base md:text-2xl lg:text-lg
+            font-bold tracking-wide"
+            >
               &quot;Hiking in Japan. Simplified.&quot;
             </h1>
           </header>
           <section>
-            <p className="text-sm md:text-lg xl:text-base font-medium">
+            <p className="text-sm md:text-lg lg:text-base font-medium">
               Experience Japan&apos;s beauty with <strong>Hikeable</strong> -
               the ultimate hiking app. Connect with fellow adventurers, leave
               trailside messages, and uncover new horizons.
@@ -72,7 +75,7 @@ function Landing() {
               onClick={() => router.push("/prefectures")}
               className={`${
                 isButtonVisible ? "opacity-100" : "opacity-0"
-              } rounded-xl font-semibold drop-shadow-md cursor-pointer bg-slate-100 text-lime-800 px-8 py-4 xl:py-2 transition-all duration-300 ease-in-out border-2 hover:bg-lime-800 hover:text-slate-100 hover:border-slate-100`}
+              } rounded-xl font-semibold drop-shadow-md cursor-pointer bg-slate-100 text-lime-800 px-8 py-4 lg:py-2 transition-all duration-300 ease-in-out border-2 hover:bg-lime-800 hover:text-slate-100 hover:border-slate-100`}
             >
               Get Started
             </button>
