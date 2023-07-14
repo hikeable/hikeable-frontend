6aa31c97d4eb66cbfe7ea5858717a4d8178b282a
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-07-14 12:31:48 +0900

    Feat: Touch Hamburger.tsx component

--- components/NewNavbar.tsx
+++ components/NewNavbar.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Mountain from "../public/mountain.svg";
+import Hamburger from "./Hamburger";
+import { useRouter } from "next/router";
 
 export default function NewNavbar() {
+  const router = useRouter();
   const navLinks = [
     {
       key: 1,
       name: "Map",
-      path: "/",
+      path: "/prefectures",
     },
     {
       key: 2,
@@ -19,20 +22,36 @@ export default function NewNavbar() {
       path: "/login",
     },
   ];
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+
+  function handleNavClick(path: string) {
+    router.push(path);
+  }
+
+  function handleHamburgerClick() {
+    setIsHamburgerOpen(!isHamburgerOpen);
+  }
 
   return (
-    <header className="bg-slate-100 w-full h-16 py-2 px-4 flex flex-row items-center justify-between drop-shadow-md">
-      <section className="flex flex-row gap-2 justify-center items-center hover:cursor-pointer">
+    <header className="bg-slate-100 w-full h-16 py-3 px-6 flex flex-row items-center justify-between drop-shadow-md">
+      <section
+        className="flex flex-row gap-2 justify-center items-center hover:cursor-pointer"
+        onClick={() => handleNavClick("/")}
+      >
         <Mountain className="fill-lime-800" />
         <span className="text-lime-800 font-semibold text-4xl">Hikeable</span>
       </section>
       <nav>
+        <button className="inline md:hidden">
+          <Hamburger isHamburgerOpen={isHamburgerOpen}/>
+        </button>
         <ul className="flex-row gap-6 hidden md:flex">
           {navLinks.map((link) => {
             return (
               <li
                 key={link.key}
-                className="hover:underline hover:cursor-pointer text-green-800 hover:text-green-700 font-semibold transition-all ease-in-out duration-50"
+                onClick={() => handleNavClick(link.path)}
+                className="hover:underline hover:cursor-pointer text-green-800 hover:text-green-700 text-lg font-semibold transition-all ease-in-out duration-50"
               >
                 {link.name}
               </li>
